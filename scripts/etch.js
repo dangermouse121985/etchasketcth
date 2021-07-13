ef413b0892d9c9872505c494ccd2324c0aa83e88
David <dcrichlo.spsu@gmail.com>
2021-07-13 19:48:48 -0400

    added ability to color individual files with mouseover. SiZed grid heigh to be proportional to width (squares)

--- scripts/etch.js
+++ scripts/etch.js
@@ -1,11 +1,11 @@
-let gridContainer = document.querySelector('#gridContainer');
+let gridContainer = document.querySelector('#grid');
 gridContainer.style.display = 'grid';
 
-let gridSize = 16;
+let gridSize = 60;
 // Create our shared stylesheet:
 const sheet = new CSSStyleSheet();
 sheet.replaceSync(
-        '#gridContainer {grid-template-columns: repeat(' + gridSize + ', 1fr);grid-template-rows: repeat(' + gridSize + ', 1fr);}');
+        '#grid {grid-template-columns: repeat(' + gridSize + ', 1fr);grid-template-rows: repeat(' + gridSize + ', 1fr);}');
 
 // Apply the stylesheet to a document:
 document.adoptedStyleSheets = [sheet];
@@ -15,3 +15,14 @@ for (let i = 0; i <= gridSize*gridSize; i++) {
     gridCell.className = 'gridCell';
     gridContainer.appendChild(gridCell);
 }
+
+let items = document.querySelectorAll('.gridCell');
+items.forEach(item => {
+    item.addEventListener('mouseover', () => {
+        item.style.backgroundColor = 'black';
+    });
+});
+
+document.querySelector('.gridCell').addEventListener('mouseover', function () {
+    document.querySelector('.gridCell').style.backgroundColor = 'black';
+});
