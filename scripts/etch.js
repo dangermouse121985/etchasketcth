4af9d3eb1b539bbf0008167b7f6aeef36cb47f30
David <dcrichlo.spsu@gmail.com>
2021-07-15 20:55:42 -0400

    fixed code so that cells change to black, and clear button works correctly. Also got random color to work but need to figure out grey scale.

--- scripts/etch.js
+++ scripts/etch.js
@@ -1,55 +1,51 @@
-let gridContainer = document.querySelector('#grid');
-gridContainer.style.display = 'grid';
-
-let gridSize = promptGridSize();
-// Create our shared stylesheet:
-const sheet = new CSSStyleSheet();
-sheet.replaceSync(
-        '#grid {grid-template-columns: repeat(' + gridSize + ', 1fr);grid-template-rows: repeat(' + gridSize + ', 1fr);}');
-
-// Apply the stylesheet to a document:
-document.adoptedStyleSheets = [sheet];
+let gridContainer = document.querySelector('#gridContainer');
+let items;
 let gridCell;
+makeGrid();
 
-for (let i = 0; i <= gridSize*gridSize - 1; i++) {
-    gridCell = document.createElement('div'); 
-    gridCell.className = 'gridCell';
-    gridContainer.appendChild(gridCell);
+function makeGrid() {
+    let grid = document.createElement('div');
+    grid.id = 'grid';
+    grid.style.display = 'grid';
+    gridContainer.appendChild(grid);
+
+    let gridSize = promptGridSize();
+    // Create our shared stylesheet:
+    const sheet = new CSSStyleSheet();
+    sheet.replaceSync(
+            '#grid {grid-template-columns: repeat(' + gridSize + ', 1fr);grid-template-rows: repeat(' + gridSize + ', 1fr);}');
+
+    // Apply the stylesheet to a document:
+    document.adoptedStyleSheets = [sheet];
+    
+
+    for (let i = 0; i <= gridSize*gridSize - 1; i++) {
+        gridCell = document.createElement('div'); 
+        gridCell.className = 'gridCell';
+        grid.appendChild(gridCell);
+    }
+
+    //Change color of cell to black with mouseover event
+    let items = document.querySelectorAll('.gridCell');
+    items.forEach(item => {
+        item.addEventListener('mouseover', () => {
+            //item.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);;
+            console.log(item.style.backgroundColor)
+            item.style.background = 'hsl(0, 0%, 0%, .1)';
+        });
+    });
 }
 
-let items = document.querySelectorAll('.gridCell');
-items.forEach(item => {
-    item.addEventListener('mouseover', () => {
-        item.style.backgroundColor = 'black';
-    });
-});
-
-document.querySelector('.gridCell').addEventListener('mouseover', function () {
-    document.querySelector('.gridCell').style.backgroundColor = 'black';
-});
 
 document.querySelector('#clearGrid').addEventListener('click', function() {
-    for (let i=0; i < items.length; i++) {
-        items[i].style.backgroundColor ='white';
-    }
-    gridCell.remove();
-    gridSize = promptGridSize();
-    sheet.replaceSync(
-        '#grid {grid-template-columns: repeat(' + gridSize + ', 1fr);grid-template-rows: repeat(' + gridSize + ', 1fr);}');
-    
-    for (let i = 0; i <= gridSize*gridSize - 1; i++) {
-        gridCell = document.createElement('div');
-        gridCell.className = 'gridCell';
-        gridContainer.appendChild(gridCell);
-    }
+    grid.remove();
+    makeGrid();
 });
 
 function promptGridSize () {
     let size = prompt('How big do you want the grid (10-100)?');
-    if (size >= 10 || size <= 100) {
-        return size;
+    while (size < 10 || size > 100) {
+        size = prompt('I\'m sorry that number is not valid. How big do you want the grid (10-100)?');
     }
-    else{
-        size = prompt('How big do you want the grid (10-100)?');
-    }
+     return size;   
 }
